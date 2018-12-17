d5f37fddb05337b8aaef129557512ce35d87c9dc
Thomas Martin <thomas@oopss.org>
2018-12-17 17:00:40 +0100

    Add first models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from model_utils.models import TimeStampedModel, SoftDeletableModel
+
+
+class BaseModel(TimeStampedModel, SoftDeletableModel):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT,
+                                related_name='+')
+
+    class Meta:
+        abstract = True
+
+
+class PlaceMap(BaseModel):
+    admin = models.ManyToManyField(User, related_name='+',
+                                   null=True, blank=True)
+    viewer = models.ManyToManyField(User, related_name='+',
+                                    null=True, blank=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(BaseModel):
+    placemap = models.ForeignKey(PlaceMap, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+
+    def __str__(self):
+        return self.name
+
+
+class Evaluation(BaseModel):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    note = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return '{} by {}'.format(self.place.name, self.creator.username)
