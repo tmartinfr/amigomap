6bbfd22800b91a7f1f7c7a1b49ca406cb3537918
Thomas Martin <thomas@oopss.org>
2018-12-26 16:48:45 +0100

    Add factories

---
+++ app/factories.py
@@ -0,0 +1,30 @@
+import factory
+from . import models
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.User
+
+    username = factory.Faker('sha1')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+
+
+class MapFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Map
+
+    creator = factory.SubFactory(UserFactory)
+    name = factory.Faker('city')
+
+
+class PlaceFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Place
+
+    creator = factory.SubFactory(UserFactory)
+    map = factory.SubFactory(MapFactory)
+    name = factory.Faker('company')
+    latitude = factory.Faker('latitude')
+    longitude = factory.Faker('longitude')
