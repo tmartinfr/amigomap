532cbf65da2fef5557975ecf52b803349d5adb23
Thomas Martin <thomas@oopss.org>
2018-12-27 08:55:48 +0100

    Add google_place_id on Place

--- app/models.py
+++ app/models.py
@@ -52,6 +52,7 @@ class Place(BaseModel):
     latitude = models.DecimalField(max_digits=9, decimal_places=7)
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
     color = ColorField()
+    google_place_id = models.CharField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
