ca6c23faae6c60d8dbc5bc90120a419f548040fa
Thomas Martin <thomas@oopss.org>
2019-02-07 17:19:20 +0100

    Allow initial setup using invoke

---
+++ tasks.py
@@ -0,0 +1,26 @@
+from invoke import task
+
+
+@task(default=True)
+def help(c):
+    """
+    Print this help message and exit
+    """
+    c.run('invoke --list')
+
+
+@task
+def populate_dev_db(c):
+    """
+    Prepare and fill development DB with random data
+    """
+    c.run('django-admin migrate')
+    c.run('django-admin app_populate_dev_db')
+
+
+@task
+def run(c):
+    """
+    Start development server
+    """
+    c.run('django-admin runserver 0.0.0.0:8000')
