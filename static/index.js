d66d3b60475027dd748d8c3e156e41fa680e7079
Thomas Martin <thomas@oopss.org>
2019-02-10 15:16:03 +0100

    Update JS for new API structure

--- static/index.js
+++ static/index.js
@@ -5,8 +5,8 @@ new Vue({
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
         axios.get('/api/map/bydomain/')
             .then(response => {
-                map_id = response.data['uuid'];
-                axios.get('/api/map/' + map_id + '/place/')
+                map_uuid = response.data['uuid'];
+                axios.get('/api/place/?map_uuid=' + map_uuid)
                     .then(response => {
                         response.data.forEach(place => {
                             L.marker([place.latitude, place.longitude]).addTo(map).bindPopup(place.name).openPopup();
