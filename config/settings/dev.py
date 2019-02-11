d59827c38607bc58bf9166c89466c8056cea38c5
Thomas Martin <thomas@oopss.org>
2019-02-11 11:59:32 +0100

    Fix lint in settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,13 +1,13 @@
-from .base import *  # noqa
+from .base import INSTALLED_APPS, MIDDLEWARE, REST_FRAMEWORK
 
 # Maps can be requested using <map_slug>.localhost.
 ALLOWED_HOSTS = [".localhost"]
 
 DEBUG = True
 
-INSTALLED_APPS += ("debug_toolbar", "django_extensions")  # noqa
+INSTALLED_APPS += ("debug_toolbar", "django_extensions")
 
-MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE  # noqa
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append(
     "rest_framework.renderers.BrowsableAPIRenderer"
