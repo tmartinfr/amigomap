821797de45387ca58659ea38f672e61e55dfb19e
Thomas Martin <thomas@oopss.org>
2019-02-27 11:45:55 +0100

    Add visibility ENUM type

---
+++ app/migrations/0007_visibility_enum.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("app", "0006_remove_place_color")]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            create type visibility as enum ('public', 'private');
+            alter table app_map alter column visibility
+                                type visibility using visibility::visibility;
+            """,
+            """
+            alter table app_map alter column visibility type character varying(32);
+            drop type visibility;
+            """,
+        )
+    ]
