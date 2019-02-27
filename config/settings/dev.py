f80da173f94face8059c753913fa991e7feac543
Thomas Martin <thomas@oopss.org>
2019-02-27 22:33:04 +0100

    Pass SECRET_KEY in env var

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,6 +14,4 @@ REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append(
     "rest_framework.renderers.BrowsableAPIRenderer"
 )
 
-SECRET_KEY = "d)1)w5(&@^a=avczu_j$y0a)!-zq%v_vfywfuzv8rq_51t)-^+"
-
 SHELL_PLUS_POST_IMPORTS = [("app.factories", "*")]
