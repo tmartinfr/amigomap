db58b2a4111aa841406a8b39712e34a4a01522ec
Thomas Martin <thomas@oopss.org>
2019-04-29 13:41:31 +0200

    Fix import order

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,6 @@ from rest_framework.routers import SimpleRouter
 
 from .viewsets import MapViewSet, PlaceListViewSet, PlaceRetrieveViewSet
 
-
 api_router = SimpleRouter()
 api_router.register("map", MapViewSet)
 api_router.register("place", PlaceListViewSet, basename="place")
