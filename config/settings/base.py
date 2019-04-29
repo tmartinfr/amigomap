8f6c69c3aef0b57a29dada84fbb0d5529616e8d9
Thomas Martin <thomas@oopss.org>
2019-04-29 08:59:57 +0200

    Add type hint on ALLOWED_HOSTS

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from typing import List
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -24,7 +26,7 @@ DEBUG = False
 
 # This setting should be overwritten with a value like ['.example.com'], so
 # maps can be requested using <map_slug>.example.com.
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
