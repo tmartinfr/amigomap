7f330663a15f3c1be1678c06fe216abe4f507b26
Thomas Martin <thomas@oopss.org>
2019-05-20 08:56:01 +0200

    Disable cov if tests fail

--- tasks.py
+++ tasks.py
@@ -74,7 +74,8 @@ def pytest(c):
     Run unit tests
     """
     c.run(
-        "pytest --ds config.settings.base --cov=app --cov-fail-under=100",
+        "pytest --ds config.settings.base "
+        "--cov=app --cov-fail-under=100 --no-cov-on-fail",
         pty=True,
     )
 
