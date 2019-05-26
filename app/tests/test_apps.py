be0f75d7f5e0b4e3fb5aa7ff75358fd8e64d8eae
Thomas Martin <thomas@oopss.org>
2019-05-26 15:20:25 +0200

    Blackify

--- app/tests/test_apps.py
+++ app/tests/test_apps.py
@@ -6,5 +6,5 @@ from app.apps import AppConfig
 
 class AppConfigTest(TestCase):
     def test_apps(self):
-        self.assertEqual(AppConfig.name, 'app')
-        self.assertEqual(apps.get_app_config('app').name, 'app')
+        self.assertEqual(AppConfig.name, "app")
+        self.assertEqual(apps.get_app_config("app").name, "app")
